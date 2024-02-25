dfbb92ee441e1fcf5d988b746511bcff23ae163d
viki1112 <156492664+viki1112@users.noreply.github.com>
2024-02-25 17:52:15 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 
 import math
-# import argparse
 
 def root(n):
-#     if n<0:
-#         raise ValueError("Cannot calculate the square root of a negative number.")
     res=math.sqrt(n)
     print(res)
     return res
 
 def factorial(n):
-    # if n<0:
-        # raise ValueError("Factorial is not defined for negative numbers.")
     res=math.factorial(n)
     print(res)
     return res
 
 def log(n):
-    # if n<=0:
-        # raise ValueError("Logarithm is not defined for non-positive numbers.")
     res=math.log(n,math.e)
     print(res)
     return res
@@ -53,24 +46,6 @@ def main():
         power(base, exponent)
     else:
         print("Invalid Operation")
-
-# def main():
-#     parser = argparse.ArgumentParser(description='Enter an operation: ')
-#     parser.add_argument('operation', type=str, choices=['1', '2', '3', '4'],
-#                         help='The operation to perform: root, factorial, log, power')
-#     parser.add_argument('number', type=float, help='The number (for root, factorial, log) or base (for pow)')
-#     parser.add_argument('--exponent', type=float, help='The exponent (required for pow operation)', default=1.0)
-
-#     args = parser.parse_args()
-
-#     if args.operation == '1':
-#         root(args.number)
-#     elif args.operation == '2':
-#         factorial(int(args.number))  # factorial expects an int
-#     elif args.operation == '3':
-#         log(args.number)
-#     elif args.operation == '4':
-#         power(args.number, args.exponent)
 
 if __name__=="__main__":
     main()
